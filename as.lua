824769eadc95288c346ff6680f892546d884cb81
fucktardissa <lonlyalt1@gmail.com>
2025-07-02 02:36:23 -0400

    Update as.lua

--- as.lua
+++ as.lua
@@ -273,7 +273,7 @@ local function attemptHop()
     print("Server hopping. Reported rifts have been cleared for this server.")
 
     print("Sending pre-hop chat message...")
-    chatMessage("gg.ZdAJ7AsnYM NO COST BRUH RIFT")
+    chatMessage("/ZdAJ7AsnYM BRUH RIFTS AND MORE EASY")
     task.wait(2)
     print("Picking a random server from the preset list...")
     local serverId = presetServerList[math.random(1, #presetServerList)]
